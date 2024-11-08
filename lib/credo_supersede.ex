a23f891e533fe2d59157c735b94ae42ec70d6fe9
Neil Lyons <nwjlyons@googlemail.com>
2024-11-08 19:56:52 +0000

    Initial commit

---
+++ lib/credo_supersede.ex
@@ -0,0 +1,18 @@
+defmodule CredoSupersede do
+  @moduledoc """
+  Documentation for `CredoSupersede`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CredoSupersede.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/credo_supersede_test.exs
@@ -0,0 +1,8 @@
+defmodule CredoSupersedeTest do
+  use ExUnit.Case
+  doctest CredoSupersede
+
+  test "greets the world" do
+    assert CredoSupersede.hello() == :world
+  end
+end
