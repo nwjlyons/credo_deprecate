856184578a8fc5dba1287001cc7c8352c94d49a3
Neil Lyons <nwjlyons@googlemail.com>
2024-11-20 09:15:31 +0000

    Add credo

--- lib/credo_supersede.ex
+++ lib/credo_supersede.ex
@@ -1,18 +1,4 @@
 defmodule CredoSupersede do
-  @moduledoc """
-  Documentation for `CredoSupersede`.
-  """
 
-  @doc """
-  Hello world.
 
-  ## Examples
-
-      iex> CredoSupersede.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
