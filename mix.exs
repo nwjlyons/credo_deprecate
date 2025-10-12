b5553be3f8d1a4bb4b3edcdd5ed6c0b8f3f8fd7b
Neil Lyons <nwjlyons@googlemail.com>
2025-10-12 16:24:28 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CredoDeprecate.MixProject do
   def project do
     [
       app: :credo_deprecate,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
